3e3638d08f34c89737d50aca5da78cf524ac48c4
Luis <luisfernando.4862@hotmail.com>
2021-07-05 23:54:06 -0300

    teste

---
+++ lib/app_widget.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+import 'package:desafio_marketeasy/home_page.dart';
+import 'package:desafio_marketeasy/products_page.dart';
+
+import 'home_page.dart';
+
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+       '/products': (context) => products_page(),
+
+      },
+    );
+  }
+}
